3f88b509c473394e3fca4685325a05e4a0f0e4bc
Omid Mogasemi <omidmogasemi@gmail.com>
2021-02-11 18:26:01 -0800

    minor fix

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 let Item = require('../models/items.model.js') 
 
-router.route('/get_all_items').get((req, res) => {
-    Item.find()
-        .then(items => res.json(items))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
-
 router.route('/add_item').post((req, res) => {
     const name = req.body.name;
     const date_added = req.body.date_added;
