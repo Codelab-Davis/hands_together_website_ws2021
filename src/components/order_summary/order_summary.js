b10a32e79f1fd6c2b6a759105508feb89f683481
Edward Chew <edwardchew8@gmail.com>
2021-03-10 23:36:45 -0800

    Display order_summary data

--- src/components/order_summary/order_summary.js
+++ src/components/order_summary/order_summary.js
@@ -6,28 +6,39 @@ import axios from "axios";
 function OrderSummary() {
   let transaction_id = useParams();
   const [transaction_data, set_transaction_data] = useState();
+  const [valid, set_valid] = useState(false);
 
   useEffect(() => {
     console.log(transaction_id);
     axios.get("http://localhost:5000/sold_items/get_sale", { params: { transaction_id: transaction_id }})
       .then(res => { 
-        transaction_data = res.data;
+        set_transaction_data(res.data);
+        set_valid(true);
         console.log(res.data);
       })
-  })
+  }, []) /* Run useEffect only once */
 
   return(
-    <>
+    <div>
       <h1>This is the order summary page</h1>
       {
-        // transaction_data.map((data, key) =>
-        //   <>
-        //     <p>data.name</p>
-        //     <p>data_added.name</p>
-        //   </>
-        // )
+        valid &&
+        transaction_data.map((data, key) =>
+          <div>
+            <p>Name: {data.name}</p>
+            <p>Images: {data.images}</p>
+            <p>Date Sold: {data.date_sold}</p>
+            <p>Tracking Link: {data.tracking_link}</p>
+            <p>Shipping Address: {data.shipping_address.city}
+              {data.shipping_address.country}
+              {data.shipping_address.line1}
+              {data.shipping_address.line2}
+              {data.shipping_address.postal_code}
+              {data.shipping_address.state}</p>
+          </div>
+        )
       }
-    </>
+    </div>
   )
 }
 
