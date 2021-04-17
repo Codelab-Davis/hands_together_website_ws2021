2b8173b5c1de30ad8dfb7ed372ea134728755239
Abhay Thacker <abhaythacker2@gmail.com>
2021-04-17 14:07:09 -0700

    gaurded admin page route

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,12 @@ import item_page from "./shop/item_page";
 import add_item from "./shop_dashboard/add_item";
 import admin_dashboard from "./shop_dashboard/admin_dashboard";
 import edit_or_remove_item from "./shop_dashboard/edit_or_remove_item";
-import login from "./shop_dashboard/login";
+import Login from "./shop_dashboard/login";
 import donation from "./donation";
 import order_summary from "./order_summary/order_summary";
 import volunteer_events from "./volunteer_events";
 import Navbar from "./navbar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import React, { useState } from 'react'; 
 import GuardedRoute from './GuardedRoute';
 
@@ -27,11 +27,11 @@ function App() {
         <Route exact path="/" component={home} />
         <Route exact path="/about" component={about} />
         <Route exact path="/shop" component={shop} />
-        <Route exact path="/login" component={login} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
+        <Route exact path="/login" render={(props) => loggedIn ? <Redirect to="/admin" /> : (<Login {...props} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />)} />
         <Route exact path="/item/:id" component={item_page} /> 
         <Route exact path="/shop/:id" component={item_page} />
-        <Route exact path="/admin" component={admin_dashboard} />
-        {/* <GuardedRoute path="/admin" component={admin_dashboard} auth={loggedIn} /> */}
+        {/* <Route exact path="/admin" component={admin_dashboard} /> */}
+        <GuardedRoute path="/admin" component={admin_dashboard} auth={loggedIn} />
         <Route exact path="/add" component={add_item} />
         <Route exact path="/edit" component={edit_or_remove_item} />
         <Route exact path="/order_summary/:transaction_id" component={order_summary} />
