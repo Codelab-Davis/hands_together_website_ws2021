059fe7213e1574b6ef557a55bc9db2ac2a25f321
Nicolelaz <nicole@lazovsky.com>
2021-04-20 16:00:49 -0700

    implemented new spacing features

--- src/components/volunteer_events.js
+++ src/components/volunteer_events.js
@@ -8,76 +8,73 @@ import Rectangle_38 from "../images/Rectangle_38.png";
 //question to future self: how do we make the volunteer & events section a pointer cursor without doing it for the whole navbar?
 
 function volunteer_events() {
-  //   const history = useHistory();
-  // BIG TO-DO FOR THIS WHOLE THING: FIGURE OUT PADDING AND SPACING T^T
   return (
-    <div>
-      {/* This is the big first image, to do: properly center it*/}
-      <div align="center" class="container-fluid p-0">
-        <div class="row no-gutters">
-          <div class="col">
-            <div className="rectangle_image" align="center">
-              <img src={Rectangle_41} />
-            </div>
-            {/* This is the sign up button */}
-            <div>
-              {/* to do: overlap sign up button with image on top */}
-              <button className="event_sign_up_button">
+    <div class="container-fluid p-0 left-space">
+      {/* upcoming events title + description block */}
+      <div>
+        <h1 className="upcoming-events-text">Upcoming Events</h1>
+        <h3 className="description-text">Description of upcoming events.</h3>
+      </div>
+
+      {/* This is the big first image */}
+      <div className="banner_image ">
+        <img src={Rectangle_41} />
+      </div>
+      {/* This is the sign up button */}
+      <div>
+        {/* to do: overlap sign up button with image on top */}
+
+        {/* <button className="event_sign_up_button">
                 <p>sign up to volunteer!</p>
-              </button>
-            </div>
-          </div>
+              </button> */}
+      </div>
+
+      {/* This is the picture tiles section */}
+
+      <div class="container-fluid p-0">
+        <div class="row no-gutters event-tile-banner-space event-tile-container">
+          <div className="event_tile" />
+          <div className="event_tile" />
+          <div className="event_tile" />
         </div>
+      </div>
 
-        {/* This is the picture tiles section */}
-
-        <div align="center" class="container-fluid p-0">
-          <div class="row no-gutters">
-            <div class="col" />
-            <div class="col" className="event_tile" />
-            <div class="col" />
-            <div class="col" className="event_tile" />
-            <div class="col" />
-            <div class="col" className="event_tile" />
-            <div class="col" />
+      {/* Volunteering opportunities text */}
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col-1" />
+          <div class="col-9">
+            <h1> Volunteering Opportunities</h1>
+            <h3 className="description-text">
+              Description of volunteering opportunities and events available.
+            </h3>
           </div>
+          <div class="col-2" />
         </div>
+      </div>
 
-        {/* Volunteering opportunities text */}
-        <div align="left" class="container-fluid p-0">
-          <div class="row no-gutters">
-            <div class="col-1" />
-            <div class="col-9">
-              <h1>Volunteering opportunities</h1>
-              <p1>
-                Description of volunteering opportunities and events available.
-              </p1>
-            </div>
-            <div class="col-2" />
-          </div>
+      {/* Event Name + Calendar */}
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col" className="event_tile2" />
+          <div class="col" className="calendar" />
+          <div class="col" />
+          <div class="col"></div>
         </div>
+      </div>
 
-        {/* Event Name + Calendar */}
-        <div align="center" class="container-fluid p-0">
-          <div class="row no-gutters">
-            <div class="col" className="event_tile2" />
-
-            <div class="col">
-              <p>calendar should be here</p>
-            </div>
+      {/* Volunteer Sign Up Form */}
+      <div align="left" class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col-1" />
+          <div class="col-8">
+            <h1>Volunteer Sign Up Form</h1>
+            <h3 className="description-text">
+              Please fill in the information below to sign up for this
+              volunteering
+            </h3>
           </div>
-        </div>
-
-        {/* Volunteer Sign Up Form */}
-        <div align="left" class="container-fluid p-0">
-          <div class="row no-gutters">
-            <div class="col-1" />
-            <div class="col-8">
-              <h1>Volunteer Sign Up Form</h1>
-              <p>the sign up form should go here</p>
-            </div>
-            <div class="col-4" />
-          </div>
+          <div class="col-4" />
         </div>
       </div>
     </div>
