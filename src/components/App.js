49d66f7991deb608eba9b543f54216c4b24af89f
Omid Mogasemi <omidmogasemi@gmail.com>
2021-04-30 13:08:28 -0700

    merge changes

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ import donation from "./donation";
 import order_summary from "./order_summary/order_summary";
 import volunteer_events from "./volunteer_events";
 import Navbar from "./navbar";
+import Footer from "./footer"; 
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import React, { useState } from 'react'; 
 import GuardedRoute from './GuardedRoute';
@@ -31,23 +32,23 @@ function App() {
           <Route exact path="/" component={home} />
           <Route exact path="/about" component={about} />
           <Route exact path="/shop" render={(props) => (<Shop {...props} 
-                                                          storageQuota={storageQuota} 
-                                                          setStorageQuota={setStorageQuota}
-                                                          cartedItems={cartedItems} 
-                                                          setCartedItems={setCartedItems} 
-                                                        />)} />
+            storageQuota={storageQuota} 
+            setStorageQuota={setStorageQuota}
+            cartedItems={cartedItems} 
+            setCartedItems={setCartedItems} 
+          />)} />
           <Route exact path="/item/:id" render={(props) => (<ItemPage {...props} 
-                                                              storageQuota={storageQuota} 
-                                                              setStorageQuota={setStorageQuota}
-                                                              cartedItems={cartedItems} 
-                                                              setCartedItems={setCartedItems} 
-                                                            />)} /> 
+            storageQuota={storageQuota} 
+            setStorageQuota={setStorageQuota}
+            cartedItems={cartedItems} 
+            setCartedItems={setCartedItems} 
+          />)} /> 
           <Route exact path="/shop/:id" render={(props) => (<ItemPage {...props} 
-                                                              storageQuota={storageQuota} 
-                                                              setStorageQuota={setStorageQuota}
-                                                              cartedItems={cartedItems} 
-                                                              setCartedItems={setCartedItems} 
-                                                            />)} />
+            storageQuota={storageQuota} 
+            setStorageQuota={setStorageQuota}
+            cartedItems={cartedItems} 
+            setCartedItems={setCartedItems} 
+          />)} />
           <Route exact path="/login" render={(props) => !loggedIn ? (<Login {...props} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />) : <Redirect to="/admin" />} />
           <Route exact path="/logout" render={(props) => (<Logout {...props} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />)}/>
           <Route exact path="/sold_items_test_routes" component={sold_items_test_routes} /> 
@@ -58,6 +59,7 @@ function App() {
           <Route exact path="/volunteer_events" component={volunteer_events} />
         </Switch>
       </Router>
+      <Footer />
     </div>
   );
 }
